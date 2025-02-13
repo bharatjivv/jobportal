b35dbc3c117197825be299b0189d0ec94edea0aa
BharatJiwnani1 <25414296-BharatJiwnani1@users.noreply.replit.com>
2025-02-13 03:58:06 +0000

    User checkpoint: Add simple "Hello" component and integrate into homepage

---
+++ pages/components/Hello.tsx
@@ -0,0 +1,10 @@
+const Hello: React.FC<> = () => {
+
+  return (
+    <div>
+      Are Hello! Koi hai...
+    </div>
+  );
+};
+
+export default Hello;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Counter from "./components/Counter";
 import styles from "../styles/Home.module.css";
+import Hello from "./components/Hello";
 
 const Home: NextPage = () => {
   return (
@@ -16,9 +17,11 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <div>
+          <Hello />
+        </div>
         <div className={styles.description}>
-          <Counter initialValue={5} />
+          <Counter initialValue={3} />
         </div>
       </main>
     </div>
